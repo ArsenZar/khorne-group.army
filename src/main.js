f1e5a6bd210e3efe151fbda2771a0c69d211ac0a
ArsenZar <arsenzar@gmail.com>
2025-12-08 21:52:55 +0200

    mobile 0.11v

--- src/main.js
+++ src/main.js
@@ -3,65 +3,55 @@ import 'normalize.css';
 import './css/styles.css';
 import { API_BASE_URL } from "./config";
 
-const btnMenu = document.querySelector(".hero_menu");
-const heroNav = document.querySelector(".hero_nav");
-const heroNavClose = document.querySelector(".hero_nav_menu");
+const items = document.querySelectorAll('.donate_left_cT_item');
 
-btnMenu.addEventListener('click', function () {
-  heroNav.classList.add("hero_nav_active");
+items.forEach(item => {
+  item.addEventListener('click', () => {
+    const category = item.dataset.category;
+    loadCategory(category);
+  });
 });
 
-heroNavClose.addEventListener('click', function () {
-  heroNav.classList.remove("hero_nav_active");
+window.addEventListener("DOMContentLoaded", () => {
+  loadCategory("drones"); // або будь-яка дефолтна
 });
 
-// donateActive
-
-
-
-async function loadLandingContent() {
+async function loadCategory(category) {
   try {
     const response = await fetch(
-      `${API_BASE_URL}/api/articles?sort=order:asc&populate=image`
+      `${API_BASE_URL}/api/articles?filters[category][$eq]=${category}&populate=image`
     );
 
-    if (!response.ok) {
-      console.error("Failed to fetch landing page data");
-      return;
-    }
+
 
     const json = await response.json();
     const data = json.data;
 
-    console.log(data);
-
-    if (!data) {
-      console.warn("No landing page data received");
+    if (!data || !data.length) {
+      console.warn("No data for this category:", category);
       return;
     }
+
+    const article = data[0];
+
 
     const donateTitle = document.getElementById("donate-title");
     const donateDesription = document.getElementById("donate-description");
     const donateImage = document.getElementById("donate-img");
 
-    if (donateTitle && data[0].title) {
-      donateTitle.textContent = data[0].title;
+    // Текст
+    donateTitle.textContent = article.title;
+    donateDesription.textContent = article.content;
+
+    // Картинка
+    const imgUrl = data[0].image.url;
+    console.log(imgUrl);
+
+    if (imgUrl) {
+      donateImage.style.backgroundImage = `url(${imgUrl})`;
     }
 
-    if (donateDesription && data[0].content) {
-      donateDesription.textContent = data[0].content;
-    }
-
-    if (donateImage && data[0].image && data[0].image.url) {
-      const linkImg = data[0].image.url; // типу "/uploads/..."
-      donateImage.style.backgroundImage = `url(${linkImg})`;
-      console.log(`url(${API_BASE_URL}${data[0].image.url})`);
-    }
-  } catch (error) {
-    console.error("Error loading landing content:", error);
+  } catch (err) {
+    console.error("Error loading category:", category, err);
   }
 }
-
-window.addEventListener("DOMContentLoaded", () => {
-  loadLandingContent();
-});
